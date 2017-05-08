bc885f410781c456735967fdb4514576bb720b78
SchadkoAO <schadko.ao@gmail.com>
2017-05-08 16:52:52 +0300

    init ios app

---
+++ index.android.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import {AppRegistry} from 'react-native';
+import Router from './lib/utils/Router';
+import { ThemeProvider } from 'react-native-material-ui';
+import { Provider } from 'react-redux';
+import { configureStore } from './lib/store';
+
+export default class Root extends Component {
+
+	render() {
+		return (
+			<Provider store={configureStore()}>
+				<ThemeProvider>
+					<Router/>
+				</ThemeProvider>
+			</Provider>
+		);
+	}
+}
+
+AppRegistry.registerComponent('mobileApp', () => Root);
